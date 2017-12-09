2fbd42449b10eb8c7231828aadc3ded277c7d235
jan van der want <jvdwant@hotmail.com>
2017-12-09 13:28:57 +0100

    update

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser')
 var logger = require('morgan');
 var mongodb = require('./config/mongo.db');
 // var auth_routes_v1 = require('./api/authentication.routes.v1');
+var memberRoutes = require('./api/member.routes');
 var config = require('./config/env/env');
 // var expressJWT = require('express-jwt');
 
@@ -68,7 +69,7 @@ app.use(function (req, res, next) {
 // Installeer de routers
 // app.use('/api/v1', auth_routes_v1);
 // app.use('/api/v1', userroutes_v1);
-// app.use('/api',shoppinglistRoutes, recipeRoutes);
+ app.use('/api', memberRoutes);
 
 // Errorhandler voor express-jwt errors
 // Wordt uitgevoerd wanneer err != null; anders door naar next().
